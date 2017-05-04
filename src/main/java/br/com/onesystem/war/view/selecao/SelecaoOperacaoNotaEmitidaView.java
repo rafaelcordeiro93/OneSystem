1f27731964906f76327a4f5aa9ef3e8d33f573a3
Rafael Rauber <rrafaelfr2@gmail.com>
2017-05-03 22:19:32 -0300

    Inicio Inclusão de Seleção de Operação de NotaEmitida e NotaRecebida.

---
+++ src/main/java/br/com/onesystem/war/view/selecao/SelecaoOperacaoNotaEmitidaView.java
@@ -0,0 +1,61 @@
+package br.com.onesystem.war.view.selecao;
+
+import br.com.onesystem.domain.Operacao;
+import br.com.onesystem.util.StringUtils;
+import br.com.onesystem.war.service.OperacaoService;
+import br.com.onesystem.war.service.impl.BasicCrudMBImpl;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@javax.enterprise.context.RequestScoped
+public class SelecaoOperacaoNotaEmitidaView extends BasicCrudMBImpl<Operacao> implements Serializable {
+
+    @Inject
+    private OperacaoService service;
+
+    @PostConstruct
+    public void init() {
+        beans = service.buscar();
+    }
+
+    @Override
+    public void abrirDialogo() {
+        exibirNaTela("selecaoOperacaoNotaEmitida");
+    }
+    
+    @Override
+    public String abrirEdicao(){
+        return "operacoes";
+    }
+    
+    @Override
+    public List<Operacao> complete(String query) {
+        List<Operacao> listaFIltrada = new ArrayList<>();
+        for (Operacao b : beans) {
+            if (StringUtils.startsWithIgnoreCase(b.getNome(), query)) {
+                listaFIltrada.add(b);
+            }
+        }
+        if (!StringUtils.containsLetter(query)) {
+            for (Operacao m : beans) {
+                if (StringUtils.startsWithIgnoreCase(m.getId().toString(), query)) {
+                    listaFIltrada.add(m);
+                }
+            }
+        }
+        return listaFIltrada;
+    }
+
+    public OperacaoService getService() {
+        return service;
+    }
+
+    public void setService(OperacaoService service) {
+        this.service = service;
+    }
+}
