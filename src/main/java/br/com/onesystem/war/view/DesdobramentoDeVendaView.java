bc4f522e930b8a100dca4c21b787e8f1d3724fd4
Rafael Cordeiro <rafaelcmsn@hotmail.com>
2017-05-17 21:51:48 -0300

    desdobramento de venda

--- src/main/java/br/com/onesystem/war/view/DesdobramentoDeVendaView.java
+++ src/main/java/br/com/onesystem/war/view/DesdobramentoDeVendaView.java
@@ -5,13 +5,21 @@
  */
 package br.com.onesystem.war.view;
 
+import br.com.onesystem.domain.Cobranca;
 import br.com.onesystem.domain.Configuracao;
+import br.com.onesystem.domain.Cotacao;
 import br.com.onesystem.domain.NotaEmitida;
 import br.com.onesystem.exception.DadoInvalidoException;
+import br.com.onesystem.reportTemplate.ValorPorCotacaoBV;
 import br.com.onesystem.war.builder.NotaEmitidaBV;
 import br.com.onesystem.war.service.ConfiguracaoService;
+import br.com.onesystem.war.service.CotacaoService;
 import br.com.onesystem.war.service.impl.BasicMBImpl;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -25,15 +33,24 @@ import org.primefaces.event.SelectEvent;
 @javax.faces.view.ViewScoped //javax.faces.view.ViewScoped;
 public class DesdobramentoDeVendaView extends BasicMBImpl<NotaEmitida, NotaEmitidaBV> implements Serializable {
 
-    private NotaEmitidaBV notaEmitidaSelecionada;
+    private NotaEmitidaBV notaEmitidaBV;
+
+    private NotaEmitida notaEmitida;
     private Configuracao configuracao;
+    private List<ValorPorCotacaoBV> cotacoes;
+    private List<Cotacao> cotacaoLista;
+    private Cotacao cotacao;
 
     @Inject
     private ConfiguracaoService configuracaoService;
 
+    @Inject
+    private CotacaoService service;
+
     @PostConstruct
     public void init() {
         iniciarConfiguracoes();
+        inicializaCotacoes();
         limparJanela();
     }
 
@@ -46,22 +63,162 @@ public class DesdobramentoDeVendaView extends BasicMBImpl<NotaEmitida, NotaEmiti
         }
     }
 
+    private void inicializaCotacoes() {
+        cotacaoLista = service.buscarCotacoesDoDiaAtual();
+        cotacoes = new ArrayList<>();
+        for (Cotacao c : cotacaoLista) {
+            cotacoes.add(new ValorPorCotacaoBV(c, null, null, null, configuracao.getMoedaPadrao()));
+        }
+    }
+
     @Override
     public void selecionar(SelectEvent event) {
-
+        Object obj = event.getObject();
+        if (obj instanceof NotaEmitida) {
+            e = new NotaEmitidaBV((NotaEmitida) obj);
+            System.out.println("nota" + e.getAFaturar() + " --  " + e.getDesconto() );
+            //notaEmitidaBV = e;
+           // notaEmitida = ((NotaEmitida) obj);
+        }
     }
 
     @Override
     public void limparJanela() {
         e = new NotaEmitidaBV();
+        notaEmitidaBV = new NotaEmitidaBV();
+        notaEmitida = new NotaEmitida();
     }
 
-    public NotaEmitidaBV getNotaEmitidaSelecionada() {
-        return notaEmitidaSelecionada;
+    public BigDecimal getTotalParcelas() {
+        BigDecimal totalParcela = BigDecimal.ZERO;
+
+        for (Cobranca p : e.getCobrancas()) {
+            if (p.getCotacao() != null && p.getCotacao() != cotacao) {
+                totalParcela = totalParcela.add(p.getValor().divide(p.getCotacao().getValor(), 2, BigDecimal.ROUND_UP));
+            } else {
+                totalParcela = totalParcela.add(p.getValor());
+            }
+        }
+
+        return totalParcela;
     }
 
-    public void setNotaEmitidaSelecionada(NotaEmitidaBV notaEmitidaSelecionada) {
-        this.notaEmitidaSelecionada = notaEmitidaSelecionada;
+    public String getTotalParcelasFormatado() {
+        BigDecimal totalParcelas = getTotalParcelas();
+
+        return totalParcelas.compareTo(BigDecimal.ZERO) == 0 ? ""
+                : NumberFormat.getCurrencyInstance(configuracao.getMoedaPadrao().getBandeira().getLocal()).format(totalParcelas);
+    }
+
+    public void geraBaixaDeCotacoes() throws DadoInvalidoException {
+        for (ValorPorCotacaoBV c : cotacoes) {
+            if (c.getValorAReceber().compareTo(BigDecimal.ZERO) > 0) {
+                e.adiciona(c.construir());//
+            }
+        }
+        // geraBoletoDeCartaoAVista();
+    }
+
+    public BigDecimal getTotalConvertidoRecebido() {
+        BigDecimal total = BigDecimal.ZERO;
+        for (ValorPorCotacaoBV c : cotacoes) {
+            total = total.add(c.getValorConvertidoRecebido());
+        }
+        return total;
+    }
+
+    public String getTotalConvertidoRecebidoFormatado() {
+        return NumberFormat.getCurrencyInstance(configuracao.getMoedaPadrao().getBandeira().getLocal()).format(getTotalConvertidoRecebido());
+    }
+
+    public NotaEmitidaBV getNotaEmitidaBV() {
+        return notaEmitidaBV;
+    }
+
+    public void setNotaEmitidaBV(NotaEmitidaBV notaEmitidaBV) {
+        this.notaEmitidaBV = notaEmitidaBV;
+    }
+
+    public NotaEmitida getNotaEmitida() {
+        return notaEmitida;
+    }
+
+    public void setNotaEmitida(NotaEmitida notaEmitida) {
+        this.notaEmitida = notaEmitida;
+    }
+
+    public void calculaValorAcrescimoEDesconto() {
+        BigDecimal total = e.getTotalItens();
+        if (total.compareTo(BigDecimal.ZERO) > 0) {
+            BigDecimal pAcrescimo = e.getPorcentagemAcrescimo() == null ? BigDecimal.ZERO : e.getPorcentagemAcrescimo();
+            BigDecimal pDesconto = e.getPorcentagemDesconto() == null ? BigDecimal.ZERO : e.getPorcentagemDesconto();
+            BigDecimal acrescimo;
+            BigDecimal desconto;
+            BigDecimal cem = new BigDecimal(100);
+
+            if (pAcrescimo.compareTo(BigDecimal.ZERO) > 0) {
+                acrescimo = (pAcrescimo.multiply(total)).divide(cem, 2, BigDecimal.ROUND_UP);
+                e.setAcrescimo(acrescimo);
+            } else {
+                e.setAcrescimo(BigDecimal.ZERO);
+            }
+            if (pDesconto.compareTo(BigDecimal.ZERO) > 0) {
+                desconto = (pDesconto.multiply((total))).divide(cem, 2, BigDecimal.ROUND_UP);
+                e.setDesconto(desconto);
+            } else {
+                e.setDesconto(BigDecimal.ZERO);
+            }
+
+            incluiValorDeFormaDeRecebimento(notaEmitida.getTotalNota());
+        }
+    }
+    
+    public void calculaPorcentagemAcrescimoEDesconto() {
+        BigDecimal total = e.getTotalItens();
+        if (total.compareTo(BigDecimal.ZERO) > 0) {
+            BigDecimal acrescimo = e.getAcrescimo() == null ? BigDecimal.ZERO : e.getAcrescimo();
+            BigDecimal desconto = e.getDesconto() == null ? BigDecimal.ZERO : e.getDesconto();
+            BigDecimal pAcrescimo;
+            BigDecimal pDesconto;
+            BigDecimal cem = new BigDecimal(100);
+
+            if (acrescimo.compareTo(BigDecimal.ZERO) > 0) {
+                pAcrescimo = (acrescimo.multiply(cem)).divide(total, 2, BigDecimal.ROUND_UP);
+                e.setPorcentagemAcrescimo(pAcrescimo);
+            } else {
+                e.setPorcentagemAcrescimo(BigDecimal.ZERO);
+            }
+            if (desconto.compareTo(BigDecimal.ZERO) > 0) {
+                pDesconto = (desconto.multiply(cem)).divide(((total)), 2, BigDecimal.ROUND_UP);
+                e.setPorcentagemDesconto(pDesconto);
+            } else {
+                e.setPorcentagemDesconto(BigDecimal.ZERO);
+            }
+
+            incluiValorDeFormaDeRecebimento(e.getTotalNota());
+        }
+    }
+
+    private void incluiValorDeFormaDeRecebimento(BigDecimal resultado) {
+        switch (e.getFormaDeRecebimento().getFormaPadraoDeEntrada()) {
+            case DINHEIRO:
+                e.setTotalEmDinheiro(resultado);
+                break;
+            case CREDITO:
+                // creditoBV.setValor(resultado);
+                break;
+            case CHEQUE:
+//                valoresAVista.setCheque(resultado);
+                break;
+            case A_FATURAR:
+                e.setAFaturar(resultado);
+                break;
+            case CARTAO:
+//                valoresAVista.setCartao(resultado);
+                break;
+            default:
+                break;
+        }
     }
 
     public Configuracao getConfiguracao() {
@@ -79,7 +236,37 @@ public class DesdobramentoDeVendaView extends BasicMBImpl<NotaEmitida, NotaEmiti
     public void setConfiguracaoService(ConfiguracaoService configuracaoService) {
         this.configuracaoService = configuracaoService;
     }
-    
-    
+
+    public List<ValorPorCotacaoBV> getCotacoes() {
+        return cotacoes;
+    }
+
+    public void setCotacoes(List<ValorPorCotacaoBV> cotacoes) {
+        this.cotacoes = cotacoes;
+    }
+
+    public List<Cotacao> getCotacaoLista() {
+        return cotacaoLista;
+    }
+
+    public void setCotacaoLista(List<Cotacao> cotacaoLista) {
+        this.cotacaoLista = cotacaoLista;
+    }
+
+    public Cotacao getCotacao() {
+        return cotacao;
+    }
+
+    public void setCotacao(Cotacao cotacao) {
+        this.cotacao = cotacao;
+    }
+
+    public CotacaoService getService() {
+        return service;
+    }
+
+    public void setService(CotacaoService service) {
+        this.service = service;
+    }
 
 }
