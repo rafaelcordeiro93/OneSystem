90cb2867a0405a034da5cb2b0ba00d8bc036c566
rauber <rauber@rauber>
2017-06-08 17:13:12 -0300

    Adequações janela de Cartão, Forma de Recebimento e Operação. Adicionado Bundle a Tipos.

--- src/main/java/br/com/onesystem/valueobjects/TipoFormaDeRecebimento.java
+++ src/main/java/br/com/onesystem/valueobjects/TipoFormaDeRecebimento.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.onesystem.valueobjects;
+
+import br.com.onesystem.util.BundleUtil;
 
 /**
  *
@@ -12,9 +13,26 @@ package br.com.onesystem.valueobjects;
  */
 public enum TipoFormaDeRecebimento {
 
-    DINHEIRO,
-    CHEQUE,
-    CARTAO,
-    A_FATURAR,
-    CREDITO
+    DINHEIRO(new Long(1), new BundleUtil().getLabel("Dinheiro")),
+    CHEQUE(new Long(2), new BundleUtil().getLabel("Cheque")),
+    CARTAO(new Long(3), new BundleUtil().getLabel("Boleto_De_Cartao")),
+    A_FATURAR(new Long(4), new BundleUtil().getLabel("A_Faturar")),
+    CREDITO(new Long(5), new BundleUtil().getLabel("Credito"));
+
+    private Long id;
+    private String nome;
+
+    private TipoFormaDeRecebimento(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
 }
